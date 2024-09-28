5fdec3a9f33c21a4a7d397f63f7fd84900c8d2f2
cristianOvando <ovandogomez60@gmail.com>
2024-09-28 15:19:32 -0600

    [add] login

---
+++ android/app/src/main/kotlin/com/example/skeletonflutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.skeletonflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/login.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:skeletonflutter/components/my_button.dart';
+import 'package:skeletonflutter/components/my_textfield.dart';
+import 'package:skeletonflutter/components/square_tile.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void signUserIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 50),
+
+              // logo
+              const Icon(
+                Icons.lock,
+                size: 100,
+              ),
+
+              const SizedBox(height: 50),
+
+              Text(
+                '¡BIENVENIDO!',
+                style: TextStyle(
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                  fontSize: 16,
+                ),
+              ),
+
+              const SizedBox(height: 25),
+
+            
+              MyTextField(
+                controller: emailController,
+                hintText: 'email',
+                obscureText: false,
+              ),
+
+              const SizedBox(height: 10),
+
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 10),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      '¿Olvidaste tu contraseña?',
+                      style: TextStyle(color: const Color.fromARGB(255, 54, 54, 54)),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 25),
+
+              MyButton(
+                onTap: signUserIn,
+              ),
+
+              const SizedBox(height: 50),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'O continuar con',
+                        style: TextStyle(color: Colors.grey[800]),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 50),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+              
+                  SquareTile(imagePath: 'lib/images/Google.png'),
+                ],
+              ),
+
+              const SizedBox(height: 50),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '¿No tienes cuenta?',
+                    style: TextStyle(color: Colors.grey[800]),
+                  ),
+                  const SizedBox(width: 4),
+                  const Text(
+                    'Registrate',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
